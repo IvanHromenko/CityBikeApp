f902ba02cbf5aa2f5b9f59ae93559f366b0912ad
IvanHromenko <ivangromenko.1003@gmail.com>
2023-05-27 15:54:23 +0300

    Station model + dbcontext

---
+++ CityBikeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CityBikeApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBikeApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)  : base(options)
+        {
+
+        }  
+
+        public DbSet<Station> Stations { get; set; }
+    }
+}
---
+++ CityBikeApp/Models/Station.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityBikeApp.Models
+{
+    public class Station
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
